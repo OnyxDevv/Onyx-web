7ea69fe20434f3a374d2287ac9d4511efdc5f024
OnyxDevv <aguilargonzalesb6@gmail.com>
2026-09-08 15:11:15 -0700

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -4,7 +4,7 @@
     | |/ |/ / / _ \/ _  / /_/ // /  
     |__/|__/_/_//_/\_,_/\____/___/
     
-    v1.6.65-iLunXPRO-MAXPERF2  |  2026-09-08  |  Roblox UI Library for scripts
+    v1.6.66-iLunXPRO-MAXPERF3  |  2026-09-08  |  Roblox UI Library for scripts
     
     To view the source code, see the `src/` folder on the official GitHub repository.
     
